75028dd00d18793dd2f77d04a32d22764b5dc778
Juho Snellman <jsnell@iki.fi>
2015-03-22 05:41:03 +0100

    Represent each card as an integer

--- red7.lisp
+++ red7.lisp
@@ -11,17 +11,19 @@
         for value from 7 downto 1
         do (setf (symbol-value color) value)))
 
-(deftype card () `(cons (member ,@*colors*)
-                        (integer 1 7)))
+;; A card is an integer between 0 and 55 (inclusive). The low 3 bits
+;; are the color, with a "0" being a dummy color that's not used in
+;; any way. The next 3 bits are the card's numeric value - 1 (0-6).
+(deftype card () `(mod 56))
 
 (defun card-color (card)
-  (car card))
+  (ldb (byte 3 0) card))
 
 (defun card-value (card)
-  (cdr card))
+  (1+ (ash card -3)))
 
 (defstruct (deck (:constructor %make-deck))
-  (cards (make-array 49)
+  (cards (make-array 49 :element-type 'card)
          :type (simple-array card (49)))
   (size 0 :type (integer 0 49)))
 
@@ -59,7 +61,8 @@
     (dotimes (value 7)
       (dolist (color *colors*)
         (setf (aref (deck-cards deck) (deck-size deck))
-              (cons color (1+ value)))
+              (logior (symbol-value color)
+                      (ash value 3)))
         (incf (deck-size deck))))
     (shuffle-deck deck)
     deck))
@@ -77,9 +80,7 @@
   (incf (deck-size deck)))
 
 (defun card-score (card)
-  (let ((base (card-value card))
-        (color (card-color card)))
-    (+ base (/ (symbol-value color) 7.0))))
+  card)
 
 (defun player-score (player type)
   (let ((canvas (sort (copy-list (player-canvas player))
@@ -94,34 +95,34 @@
                        (setf best-matching-card (card-score card)))))
                  (+ best-matching-card (* 100 matching-cards)))))
       (ecase type
-        (red
+        (#.red
          (card-score (first canvas)))
-        (orange
+        (#.orange
          (loop for value from 1 to 7
                maximize (score-for-condition
                          (lambda (card)
                            (eq value (card-value card))))))
-        (yellow
+        (#.yellow
          (loop for color in *colors*
                maximize (score-for-condition
                          (lambda (card)
                            (eq color (card-color card))))))
-        (green
+        (#.green
          (score-for-condition (lambda (card)
                                 (evenp (card-value card)))))
-        (blue
+        (#.blue
          (let ((colors-seen (make-array 7 :initial-element nil))
                (color-count 0)
                (best-card nil))
            (dolist (card canvas)
-             (let ((index (1- (symbol-value (card-color card)))))
+             (let ((index (1- (card-color card))))
                (unless (aref colors-seen index)
                  (setf (aref colors-seen index) t)
                  (incf color-count)
                  (unless best-card
                    (setf best-card (card-score card))))))
            (+ (* 100 color-count) best-card)))
-        (indigo
+        (#.indigo
          (let ((prev nil)
                (current-run-score 0)
                (best-score 0))
@@ -138,14 +139,14 @@
                     (setf prev card)))
              (setf best-score (max best-score current-run-score)))
            best-score))
-        (violet
+        (#.violet
          (score-for-condition (lambda (card)
                                 (< (card-value card) 4))))))))
 
 (defun who-is-winning (game)
   (let ((type (if (game-discard game)
                   (card-color (first (game-discard game)))
-                  'red))
+                  #.red))
         ;; Note: all scoring types must score 0 when the canvas doesn't match
         ;; at all.
         (best-score 0)
